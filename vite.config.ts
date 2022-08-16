60de4360e8e167423f338a7cd2eb6cda534e2d9d
陈靖 <jing.chen@xbongbong.com>
2022-08-16 15:13:10 +0800

    feat: 03课

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from '@vitejs/plugin-vue-jsx';
+// import Unocss from "unocss/vite";
+import Unocss from "./config/unocss";
+
+// https://vitejs.dev/config/
+
+const rollupOptions = {
+
+  external: ["vue", "vue-router"],
+  output: {
+    globals: {
+      vue: "Vue",
+    },
+  },
+};
+
+export default defineConfig({
+
+  plugins: [
+    vue(),
+    vueJsx(),
+    // 添加UnoCSS插件
+    Unocss()
+  ],
+
+  build: {
+    rollupOptions,
+    minify:false,
+    lib: {
+      entry: "./src/entry.ts",
+      name: "SmartyUI",
+      fileName: "smarty-ui",
+      // 导出模块格式
+      formats: ["esm", "umd", "iife"],
+    },
+    cssCodeSplit: true
+  }
+});
