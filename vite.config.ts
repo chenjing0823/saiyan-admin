1c517837e7a672b07a86606c782b4e8d3142b770
陈靖 <jing.chen@xbongbong.com>
2022-08-18 15:43:24 +0800

    feat: 打包

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,9 @@ export default defineConfig({
 
   build: {
     rollupOptions,
-    minify:false,
+    minify: 'terser', // boolean | 'terser' | 'esbuild'
+    sourcemap: true, // 输出单独 source文件
+    brotliSize: true,  // 生成压缩大小报告
     lib: {
       entry: "./src/entry.ts",
       name: "SmartyUI",
