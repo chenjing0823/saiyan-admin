d0ce51b08b412aceb8c5ab6f12010407e28bfe9d
陈靖 <jing.chen@xbongbong.com>
2022-08-23 15:53:50 +0800

    feat: 目录修改

--- packages/saiyan-ui/src/button/index.ts
+++ packages/saiyan-ui/src/button/index.ts
@@ -1,13 +1,3 @@
 import Button from "./Button";
-import { App } from "vue";
 
-// export default Button;
-// 导出Button组件
-export { Button };
-
-// 导出Vue插件
-export default {
-  install(app: App) {
-    app.component(Button.name, Button);
-  },
-};
+export default Button;
