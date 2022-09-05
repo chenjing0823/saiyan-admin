6cf58ddbd9975c3a38c7464186f73e38589eb99f
陈靖 <jing.chen@xbongbong.com>
2022-09-05 13:41:44 +0800

    feat: 优化play

--- play/app.vue
+++ play/app.vue
@@ -3,53 +3,27 @@
   <s-button color="pink">红色按钮</s-button>
   <s-input v-model="input" clearable></s-input>
   {{ input }}
-  <div class="2112" style="display:flex;width: 400px">
-    <s-popover
-      placement="left"
-      trigger="click"
-      title="这是标题"
-      content="这是内容">
-      <template #reference>
-        <s-button color="pink">hover</s-button>
-      </template>
-    </s-popover>
-    <s-popover
-      placement="left"
-      trigger="click"
-      title="这是标题"
-      content="这是内容">
-      <template #content>12312</template>
-      <template #reference>
-        <s-button color="pink">click</s-button>
-      </template>
-    </s-popover>
-    <s-popover
-      placement="left"
-      trigger="manual"
-      title="这是标题"
-      content="这是内容"
-      v-model="show">
-      <template #content>12312</template>
-      <template #reference>
-        <s-button color="pink" @click="show = !show">manual</s-button>
-        <div style="background-color: aliceblue;" @click="show = !show">
-          <div style="width:200px;height: 100px"></div>
-          <!-- <s-button color="pink">manual</s-button> -->
-        </div>
-      </template>
-    </s-popover>
+  <div class="box">
+    <popover></popover>
   </div>
   <s-tooltips></s-tooltips>
 </template>
 
 <script lang="ts" setup>
 
-
 const getNum = (num: number) => {
   console.log(num)
 }
 import { ref } from "vue"
+import popover from './components/popover.vue'
 const input = ref('123')
-const show = ref(false)
 
 </script>
+
+<style>
+.box {
+  display:flex;
+  width: 100%;
+  justify-content: space-around;
+}
+</style>
---
+++ play/components/popover.vue
@@ -0,0 +1,42 @@
+<template>
+    <s-popover
+      placement="top"
+      trigger="hover"
+      title="这是标题"
+      content="这是内容">
+      <template #reference>
+        <s-button color="pink">hover</s-button>
+      </template>
+    </s-popover>
+    <s-popover
+      placement="top"
+      trigger="click"
+      title="这是标题"
+      content="这是内容">
+      <template #content>12312</template>
+      <template #reference>
+        <s-button color="pink">click</s-button>
+      </template>
+    </s-popover>
+    <s-popover
+      placement="top"
+      trigger="manual"
+      title="这是标题"
+      content="这是内容"
+      v-model="show">
+      <template #content>12312</template>
+      <template #reference>
+        <s-button color="pink" @click="show = !show">manual</s-button>
+        <div style="background-color: aliceblue;" @click="show = !show">
+          <div style="width:200px;height: 100px"></div>
+          <!-- <s-button color="pink">manual</s-button> -->
+        </div>
+      </template>
+    </s-popover>
+</template>
+<script lang="ts" setup>
+
+import { ref } from "vue"
+const show = ref(false)
+
+</script>
